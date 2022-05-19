478093e35bde1dba4517472e37bbac824fe43b05
Fabrizzio Sima <cafasicha@hotmail.com>
2022-05-19 18:45:34 -0500

    enemyFollow

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -11,25 +11,37 @@ public class EnemyController : MonoBehaviour
     private bool isRunning = false;
     public float speed;
 
+    private Rigidbody2D mRigidbody;
     public HeroController hero;
+    public Transform heroTransform;
+    private Vector2 movement;
 
     private void Start()
     {
         mHealth = maxHealth;
+        mRigidbody = this.GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
-        if (isRunning)
+        if (isRunning && hero != null)
         {
-            transform.position += speed * Time.deltaTime * Vector3.left;
+            Vector3 heroPosition = new Vector3(heroTransform.position.x, transform.position.y, 0f);
+            Vector3 position = new Vector3(transform.position.x, 0f, 0f);
+            Vector3 direction = heroPosition - position;
+            direction.Normalize();
+            movement = direction;
         }
+        if (isRunning && hero == null)
+        {
+            speed = 0f;
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Fireball"))
         {
-            //Hubo una colision
             mHealth -= maxHealth * 0.25f;
             healthbar.value -= 0.25f;
 
@@ -42,24 +54,20 @@ public class EnemyController : MonoBehaviour
         }
         
     }
+
+    private void FixedUpdate()
+    {
+        MoveEnemy(movement);
+    }
+
+    private void MoveEnemy(Vector2 direction)
+    {
+        mRigidbody.MovePosition((Vector2)transform.position + (speed * Time.deltaTime * direction));
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Paso checkpoint");
         isRunning = true;
-        /*
-        if (collision.gameObject.CompareTag("TriggerLeft"))
-        {
-            Debug.Log("Paso por izq");
-            isRunning = true;
-        }
-
-        if (collision.gameObject.CompareTag("TriggerRight"))
-        {
-            Debug.Log("Paso por der");
-            speed = speed * -1f;
-            isRunning = true;
-        }
-        */
     }
-
 }
